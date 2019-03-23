0063dd75b41f86b1f1840bea072d838293447bd8
wj <342153912@qq.com>
2019-03-23 21:20:00 +0800

    test

--- src/components/Plane.vue
+++ src/components/Plane.vue
@@ -12,7 +12,7 @@
           @click="option(row)"
         >
           <el-popover placement="top" width="160" v-model="visible2">
-            <p>这是一段内容这是一段内容确定删除吗？</p>
+            <p>这是一段内容这是一段内容确定删除吗</p>
             <div style="text-align: right; margin: 0">
               <el-button size="mini" type="text" @click="visible2 = false">取消</el-button>
               <el-button type="primary" size="mini" @click="visible2 = false">确定</el-button>
